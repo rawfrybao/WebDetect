aeb6b64511e733f4fb9ceb41dd4dd8a97441fad3
rawfrybao <rawfrybao@gmail.com>
2024-07-31 13:43:59 +0800

    chore: Update webhook structures to use omitempty for optional fields

--- internal/webhook/structures.go
+++ internal/webhook/structures.go
@@ -3,8 +3,8 @@ package webhook
 type TgMessage struct {
 	ChatID          int64                     `json:"chat_id"`
 	Text            string                    `json:"text"`
-	ParseMode       string                    `json:"parse_mode"`
-	ReplyParameters *TgMessageReplyParameters `json:"reply_parameters"`
+	ParseMode       string                    `json:"parse_mode,omitempty"`
+	ReplyParameters *TgMessageReplyParameters `json:"reply_parameters,omitempty"`
 }
 
 type TgMessageReplyParameters struct {
